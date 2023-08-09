3b67ed2c5ba04c1d00d32d180edc56794e249c13
gkhns89 <gokhansisman@outlook.com>
2023-08-09 15:47:43 +0300

    Favoriye ekleme tamamlandı.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,9 @@ const App = (props) => {
       });
   };
 
-  const addToFavorites = (movie) => {};
+ const addToFavorites = (movie) => {
+   setFavoriteMovies([...favoriteMovies, movie]);
+ };
 
   return (
     <div>
@@ -61,7 +63,10 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie deleteMovie={deleteMovie} />
+              <Movie
+                deleteMovie={deleteMovie}
+                addToFavorites={addToFavorites}
+              />
             </Route>
 
             <Route path="/movies">
